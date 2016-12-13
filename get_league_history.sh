57920b916c455408387c2b6aaafaabf8a4fe1bb7
anyshpm <anyshpm@anyshpm.com>
2016-12-13 21:44:10 +0800

    Update get_league_history.sh

--- get_league_history.sh
+++ get_league_history.sh
@@ -2,12 +2,14 @@
 
 LEAGUE_ID=$1
 
+KEY=$2
+
 REMAINING=1
 MIN_ID=0
 while [[ $REMAINING -gt 0 ]]
 do
     rm -f match_history.txt
-    wget -O match_history.txt 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=DE2A619FF470C60499CF226048861466&league_id='$LEAGUE_ID'&start_at_match_id='$MIN_ID
+    wget -O match_history.txt 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key='$KEY'&league_id='$LEAGUE_ID'&start_at_match_id='$MIN_ID
     MIN_ID=$(cat match_history.txt | grep match_id | sed 's/.*://g' | sed 's/,//g' | sort | head -n 1)
     REMAINING=$(cat match_history.txt | grep results_remaining | sed 's/.*://g' | sed 's/,//g')
     cat match_history.txt | grep match_id | sed 's/.*://g' | sed 's/,//g' | sort >> match_id.tmp
